3f25897ea4e1a34d6530b28bb528243fa7c41330
Mike Barlow <mike@mikebarlow.co.uk>
2016-08-23 20:24:41 +0000

    More tests added

--- tests/ArrayHelperTest.php
+++ tests/ArrayHelperTest.php
@@ -64,9 +64,25 @@ class ArrayHelperTest extends \PHPUnit_Framework_TestCase
 
     public function testSplitArraySetsCorrectArray()
     {
-        $arr = ['this', 'is', 'a', 'big', 'array', 'to', 'split', 'up'];
+        $ArrayHelper = new ArrayHelper([]);
 
-        $ArrayHelper = new ArrayHelper($arr);
+        $ArrayHelper->help(['small', 'array']);
+        $ArrayHelper->splitArray(3);
+
+        $this->assertSame(
+            [
+                [
+                    0 => 'small'
+                ],
+                [
+                    1 => 'array'
+                ]
+            ],
+            $ArrayHelper->output()
+        );
+
+        $arr = ['this', 'is', 'a', 'big', 'array', 'to', 'split', 'up'];
+        $ArrayHelper->help($arr);
         $ArrayHelper->splitArray(4);
 
         $this->assertSame(
@@ -109,7 +125,57 @@ class ArrayHelperTest extends \PHPUnit_Framework_TestCase
             ],
             $ArrayHelper->output()
         );
+
+        $newArr = [1, 2, 3, 4, 5, 6, 7];
+        $ArrayHelper->help($newArr);
+        $ArrayHelper->splitArray(3);
+
+        $this->assertSame(
+            [
+                [
+                    0 => 1,
+                    1 => 2,
+                    2 => 3
+                ],
+                [
+                    3 => 4,
+                    4 => 5
+                ],
+                [
+                    5 => 6,
+                    6 => 7
+                ]
+            ],
+            $ArrayHelper->output()
+        );
     }
 
+    public function testClearArrayRemovesEmptyElements()
+    {
+        $ArrayHelper = new ArrayHelper(['', 'foo', null, 'bar', 'foobar', 'test', '0']);
+        $ArrayHelper->clearArray();
 
+        $this->assertSame(
+            [1 => 'foo', 3 => 'bar', 4 => 'foobar', 5 => 'test'],
+            $ArrayHelper->output()
+        );
+
+        $ArrayHelper->help([
+            'foo', '', 'test', [
+                'hello', '', 'bye'
+            ]
+        ]);
+        $ArrayHelper->clearArray();
+
+        $this->assertSame(
+            [
+                0 => 'foo',
+                2 => 'test',
+                3 => [
+                    0 => 'hello', 2 => 'bye'
+                ]
+            ],
+            $ArrayHelper->output()
+        );
+    }
 }
